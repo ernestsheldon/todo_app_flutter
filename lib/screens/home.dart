6ffcd092875b5029eb501f109d7cf4b0aabb2900
Lahiru Mahagamage <lahiru.nc@gmail.com>
2021-10-09 15:19:01 +1100

    final

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_state.dart';
 import 'package:get/instance_manager.dart';
 import 'package:get/route_manager.dart';
 import 'package:todo_app/controllers/todo_controller.dart';
@@ -12,41 +13,100 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final _formKey = GlobalKey<FormState>();
-  TextEditingController _taskController = TextEditingController();
+  final TextEditingController _taskController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    final todoController = Get.find<TodoController>();
+    return GetBuilder<TodoController>(
+      init: TodoController(),
+      initState: (_) {},
+      builder: (todoController) {
+        todoController.getData();
 
-    return Scaffold(
-      body: const Center(
-        child: Text('Home'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () => Get.defaultDialog(
-          title: 'Add Todo',
-          content: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _taskController,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Cannot be empty';
-                    }
-                    return null;
-                  },
-                ),
-                ElevatedButton(
-                  onPressed: () async => await todoController.addTodo(
-                      _taskController.text.trim(), false),
-                  child: const Text('Save'),
-                )
-              ],
-            ),
+        return Scaffold(
+          body: Center(
+            child: todoController.isLoading
+                ? const SizedBox(
+                    child: CircularProgressIndicator(),
+                  )
+                : ListView.builder(
+                    itemCount: todoController.taskList.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: Checkbox(
+                            onChanged: (value) => todoController.addTodo(
+                                todoController.taskList[index].task,
+                                !todoController.taskList[index].isDone,
+                                todoController.taskList[index].id),
+                            value: todoController.taskList[index].isDone),
+                        title: Text(todoController.taskList[index].task),
+                        trailing: SizedBox(
+                          width: 100,
+                          child: Row(
+                            children: [
+                              IconButton(
+                                onPressed: () => addTaskDialog(
+                                    todoController,
+                                    'Update Task',
+                                    todoController.taskList[index].id,
+                                    todoController.taskList[index].task),
+                                icon: const Icon(Icons.edit),
+                              ),
+                              IconButton(
+                                onPressed: () => todoController.deleteTask(
+                                    todoController.taskList[index].id),
+                                icon: const Icon(Icons.delete),
+                                color: Colors.red,
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
           ),
+          floatingActionButton: FloatingActionButton(
+            child: const Icon(Icons.add),
+            onPressed: () async =>
+                await addTaskDialog(todoController, 'Add Todo', '', ''),
+          ),
+        );
+      },
+    );
+  }
+
+  addTaskDialog(TodoController todoController, String title, String id,
+      String task) async {
+    if (task.isNotEmpty) {
+      _taskController.text = task;
+    }
+
+    Get.defaultDialog(
+      title: title,
+      content: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              controller: _taskController,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Cannot be empty';
+                }
+                return null;
+              },
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                await todoController.addTodo(
+                    _taskController.text.trim(), false, id);
+
+                _taskController.clear();
+                Get.back();
+              },
+              child: const Text('Save'),
+            ),
+          ],
         ),
       ),
     );
